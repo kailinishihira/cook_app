00148ef432846c80ae682e532436d3c1d366cef1
Andrew Parks and Kaili Nishihira <andrew@>
2017-07-03 15:38:58 -0700

    Add cooking calculator

---
+++ js/cook.js
@@ -0,0 +1,6 @@
+var liter = parseInt(prompt("Please enter number of gallons")) * 3.78541;
+
+alert(liter + " liters");
+
+var teaspoon = parseInt(prompt("Please enter tablespoons")) * 3;
+alert(teaspoon + " teaspoon");
