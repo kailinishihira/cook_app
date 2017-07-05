0f6367389266c23bb05fc789e466a002374afda3
Kaili Nishihira and Robert Murray <kaili@>
2017-07-05 16:47:04 -0700

    Add initial user interface formatting.

--- js/cook.js
+++ js/cook.js
@@ -1,6 +1,18 @@
+// Business logic
+
 var liter = parseInt(prompt("Please enter number of gallons")) * 3.78541;
 
 alert(liter + " liters");
 
 var teaspoon = parseInt(prompt("Please enter tablespoons")) * 3;
-alert(teaspoon + " teaspoon");
+
+
+// User interface logic
+
+
+
+//alert(teaspoon + " teaspoon");
+$(document).ready(function() {
+
+
+});
